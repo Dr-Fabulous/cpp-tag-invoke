a2b4c98cbfa25cb5bb8a2c45d99221345c70d464
Dr-Fabulous <71410010+Dr-Fabulous@users.noreply.github.com>
2021-09-18 13:43:13 +0100

    added cmake files, plus basic implementation

---
+++ src/inc/fb/tag_invoke.hpp
@@ -0,0 +1,66 @@
+#ifndef FB_TAG_INVOKE_HPP
+#define FB_TAG_INVOKE_HPP
+
+namespace fb {
+	namespace tag_invoke_detail {
+		template <typename T> struct remove_reference { using type = T; };
+		template <typename T> struct remove_reference<T&> { using type = T; };
+		template <typename T> struct remove_reference<T&&> { using type = T; };
+		template <typename T> using remove_reference_t = typename remove_reference<T>::type;
+
+		template <typename T>
+		constexpr T&& forward(remove_reference_t<T>& t) noexcept {
+			return static_cast<T&&>(t);
+		}
+
+		template <typename T>
+		constexpr T&& forward(remove_reference_t<T>&& t) noexcept {
+			return static_cast<T&&>(t);
+		}
+
+		template <typename T>
+		constexpr T&& declval() noexcept;
+	}
+
+	namespace tag_invoke_cpo {
+		constexpr inline struct tag_invoke_t {
+			template <typename CPO, typename... Args>
+			constexpr auto operator()(CPO&& cpo, Args&&... args) const noexcept(noexcept(tag_invoke(tag_invoke_detail::declval<CPO>(), tag_invoke_detail::declval<Args>()...)))
+											 -> decltype(tag_invoke(tag_invoke_detail::forward<CPO&&>(cpo), tag_invoke_detail::forward<Args&&>(args)...))
+			{
+				return tag_invoke(tag_invoke_detail::forward<CPO&&>(cpo), tag_invoke_detail::forward<Args&&>(args)...);
+			}
+		} tag_invoke = {};
+
+		template <typename CPO, typename... Args>
+		using tag_invoke_result_t = decltype(tag_invoke(tag_invoke_detail::declval<CPO>(), tag_invoke_detail::declval<Args>()...));
+	}
+
+	using tag_invoke_cpo::tag_invoke;
+	using tag_invoke_cpo::tag_invoke_result_t;
+
+	namespace tag_invoke_detail {
+		template <typename I, I val>
+		struct integral_constant {
+			static constexpr I value = val;
+		};
+
+		using true_type = integral_constant<bool, true>;
+		using false_type = integral_constant<bool, false>;
+
+		template <typename CPO, typename... Args>
+		constexpr auto try_tag_invoke(int) noexcept(noexcept(tag_invoke(declval<CPO>(), declval<Args>()...)))
+						-> decltype(tag_invoke(declval<CPO>(), declval<Args>()...), true_type{});
+
+		constexpr auto try_tag_invoke(...) noexcept(false)
+						-> false_type;
+	}
+
+	template <typename CPO, typename... Args>
+	inline constexpr bool is_tag_invocable_v = decltype(tag_invoke_detail::try_tag_invoke<CPO, Args...>)::value;
+
+	template <typename CPO, typename... Args>
+	inline constexpr bool is_nothrow_tag_invocable_v = noexcept(tag_invoke_detail::try_tag_invoke<CPO, Args...>);
+}
+
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,41 @@
+#include <utility>
+#include "fb/tag_invoke.hpp"
+#include "test.hpp"
+
+constexpr struct test_cpo_t {
+	template <typename T>
+	friend int tag_invoke(test_cpo_t, const T&) noexcept {
+		return 1;
+	}
+
+	template <typename T>
+	constexpr auto operator()(T&& t) const noexcept(fb::is_nothrow_tag_invocable_v<test_cpo_t, T&&>)
+						     -> fb::tag_invoke_result_t<test_cpo_t, T&&>
+	{
+		return fb::tag_invoke(*this, std::forward<T&&>(t));
+	}
+} test_cpo = {};
+
+struct foo {
+	friend int tag_invoke(test_cpo_t, foo const&) {
+		return 2;
+	}
+};
+
+struct bar {
+	friend int tag_invoke(test_cpo_t, bar const&) {
+		return 3;
+	}
+};
+
+constinit auto tests = std::tuple{
+	[](){
+		test::assert(test_cpo(nullptr) == 1);
+		test::assert(test_cpo(foo{}) == 2);
+		test::assert(test_cpo(bar{}) == 3);
+	}
+};
+
+int main(int argc, char const* args[]) {
+	return test::run(tests);
+}
